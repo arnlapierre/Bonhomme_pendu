f50b157e9a95b05b13a9aefffe7a4493282aef9b
Arnaud Lapierre <arnaud.lapierre.1@ulaval.ca>
2021-08-19 11:59:42 -0400

    fichier pris du générateur de mot pour mieux gérer le choix du mot pour le jeu et la difficulté de la partie.

--- class_mot_aléatoire_pendu.py
+++ class_mot_aléatoire_pendu.py
@@ -7,15 +7,6 @@ class MotAleatoire:
     def __init__(self):
         self.dt = pd.read_csv("Lexique_modifié.csv", sep=";")
 
-    def afficher_catégories(self):
-        # Retourne :
-        # ['NOM', 'AUX', 'VER', 'ADV', 'PRE', 'ADJ', 'ONO', 'CON', 'ART', 'PRO']
-        catégories = []
-        for i in self.dt.classe:
-            if str(i)[:3] not in catégories:
-                catégories.append(str(i)[:3])
-        return catégories[:10]
-
     def set_categorie(self, gram):
         catégorie_df = self.dt[self.dt["classe"].str[0:3] == gram]
         return catégorie_df
@@ -29,19 +20,6 @@ class MotAleatoire:
         nblettres_df = dataframe[dataframe.nblettres == nb]
         return nblettres_df
 
-    def set_nbsyllabes(self, syl, dataframe):
-        # Détermine un mot en fonction du nb de syllabes (entre 1 et 9 syllabes).
-        nbsyllabes_df = dataframe[dataframe.nbsyll == syl]
-        return nbsyllabes_df
-
-    def set_genre(self, genre, dataframe):
-        # TODO
-        pass
-
-    def set_nombre(self, nombre, dataframe):
-        # TODO
-        pass
-
     def freq_films_greater_than(self, freq, dataframe):
         rarete_films_df = dataframe[dataframe.freqfilms >= float(freq)]
         return rarete_films_df
@@ -51,7 +29,7 @@ class MotAleatoire:
         return rarete_films_df
 
     def set_all(self, *, cat=None, premlettre=None, nblettres=None,
-                nbsyllabes=None, freq_lt=None, freq_ut=None):
+                freq_lt=None, freq_ut=None):
         # catégories :
         # ['NOM', 'AUX', 'VER', 'ADV', 'PRE', 'ADJ', 'ONO', 'CON', 'ART', 'PRO']
         if cat:
@@ -62,8 +40,6 @@ class MotAleatoire:
             df = self.set_premlettre(premlettre, df)
         if nblettres:
             df = self.set_nblettres(nblettres, df)
-        if nbsyllabes:
-            df = self.set_nbsyllabes(nbsyllabes, df)
         if freq_lt or freq_ut:
             if freq_ut:
                 df = self.freq_films_greater_than(freq_ut, df)
